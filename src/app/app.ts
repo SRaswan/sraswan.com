148520f192e806203c32f5770d1dfb1d50f94426
SRaswan <shauryaraswan@gmail.com>
2025-09-20 23:33:43 -0700

    lol

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, FontAwesomeModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
