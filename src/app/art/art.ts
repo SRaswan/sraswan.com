a36f475929c65f486eab8c1c89b541311cd82105
SRaswan <shauryaraswan@gmail.com>
2025-09-21 19:42:35 -0700

    nearly done

--- src/app/art/art.ts
+++ src/app/art/art.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 interface Artwork {
   filename: string;
@@ -10,20 +10,23 @@ interface Artwork {
 @Component({
   selector: 'app-art',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './art.html',
   styleUrls: ['./art.css']
 })
-export class Art implements OnInit {
-
+export class Art implements OnInit, OnDestroy {
   items: Artwork[] = [];
   active?: Artwork;
+
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.http.get<Artwork[]>('/artworks.json').subscribe({
-      next: list => this.items = list.filter(item => item.include),
+      next: list => {
+        this.items = list.filter(item => item.include);
+        // after items are set, schedule layout
+      },
       error: err => console.log('Failed to load artworks', err)
     });
   }
@@ -40,5 +43,8 @@ export class Art implements OnInit {
   clear() {
     this.active = undefined;
   }
-  
+
+  ngOnDestroy() {
+    // Nothing to clean up for CSS-only layout
+  }
 }
