bb2b10bfa0b8281b4994888015f4f2fe2c7cb5cd
SRaswan <shauryaraswan@gmail.com>
2025-09-25 20:46:36 -0700

    no hash

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { routes } from './app/app.routes';
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
-bootstrapApplication(App, {
-  providers: [
-    provideRouter(routes),
-    { provide: LocationStrategy, useClass: HashLocationStrategy }
-  ]
-});
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
