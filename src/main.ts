738ea9b9856fffcc32f9b58a6165f4a7c09286a3
SRaswan <shauryaraswan@gmail.com>
2025-10-04 01:00:36 -0700

    no hash routing

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,10 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+
 
 bootstrapApplication(App, {
   providers: [
-    provideRouter(routes),
-    { provide: LocationStrategy, useClass: HashLocationStrategy }
+    provideRouter(routes)
   ]
 });
